1964627b89bc926fb04523d6112e401c1b6b486c
Ivan <i.romanov@protonmail.com>
2020-03-18 20:25:10 +0300

    Updates

---
+++ precache-manifest.29ea9c74fbd8a9099d2b025008748995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f25f81d0b4930ad20265a7b578eace1",
+    "url": "/health-adv/index.html"
+  },
+  {
+    "revision": "f852d47b854306a38d5b",
+    "url": "/health-adv/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "70caea2361bc836f02ec",
+    "url": "/health-adv/static/css/main.c3ceb975.chunk.css"
+  },
+  {
+    "revision": "f852d47b854306a38d5b",
+    "url": "/health-adv/static/js/2.8e911bb6.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/health-adv/static/js/2.8e911bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70caea2361bc836f02ec",
+    "url": "/health-adv/static/js/main.eed5b0c5.chunk.js"
+  },
+  {
+    "revision": "58843b6e9ce9ffe2cded",
+    "url": "/health-adv/static/js/runtime-main.cdf2c561.js"
+  }
+]);
