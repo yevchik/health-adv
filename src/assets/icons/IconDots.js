f7a4df12215672509d2cba8f017caa9c3125c295
Ivan <i.romanov@protonmail.com>
2020-03-21 16:58:25 +0300

    Add desktop video banner

---
+++ src/assets/icons/IconDots.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+const IconDots = ({ className }) => (
+  <svg className={className} viewBox="0 0 155 155" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g opacity="0.4">
+      <circle cx="24.0513" cy="45.4312" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="24.0513" cy="130.948" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="109.569" cy="45.4312" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="109.569" cy="130.948" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="2.67241" cy="45.4312" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="2.67241" cy="130.948" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="88.19" cy="45.4312" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="88.19" cy="130.948" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="24.0513" cy="66.8101" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="24.0513" cy="152.328" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="109.569" cy="66.8101" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="109.569" cy="152.328" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="2.67241" cy="66.8101" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="2.67241" cy="152.328" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="88.19" cy="66.8101" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="88.19" cy="152.328" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="24.0513" cy="2.67241" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="24.0513" cy="88.19" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="109.569" cy="2.67241" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="109.569" cy="88.19" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="66.8111" cy="2.67241" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="66.8111" cy="88.19" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="152.327" cy="2.67241" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="152.327" cy="88.19" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="66.8111" cy="45.4312" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="66.8111" cy="130.948" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="152.327" cy="45.4312" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="152.327" cy="130.948" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="2.67241" cy="2.67241" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="2.67241" cy="88.19" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="88.19" cy="2.67241" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="88.19" cy="88.19" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="45.4302" cy="2.67241" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="45.4302" cy="88.19" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="130.948" cy="2.67241" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="130.948" cy="88.19" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="45.4302" cy="45.4312" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="45.4302" cy="130.948" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="130.948" cy="45.4312" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="130.948" cy="130.948" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="24.0513" cy="24.0513" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="24.0513" cy="109.569" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="109.569" cy="24.0513" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="109.569" cy="109.569" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="66.8111" cy="24.0513" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="66.8111" cy="109.569" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="152.327" cy="24.0513" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="152.327" cy="109.569" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="66.8111" cy="66.8101" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="66.8111" cy="152.328" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="152.327" cy="66.8101" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="152.327" cy="152.328" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="2.67241" cy="24.0513" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="2.67241" cy="109.569" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="88.19" cy="24.0513" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="88.19" cy="109.569" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="45.4302" cy="24.0513" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="45.4302" cy="109.569" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="130.948" cy="24.0513" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="130.948" cy="109.569" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="45.4302" cy="66.8101" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="45.4302" cy="152.328" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="130.948" cy="66.8101" r="2.67241" fill="#DCDFE6"/>
+      <circle cx="130.948" cy="152.328" r="2.67241" fill="#DCDFE6"/>
+    </g>
+  </svg>
+)
+
+export default IconDots
