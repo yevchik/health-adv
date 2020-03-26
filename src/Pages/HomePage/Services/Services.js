06e2d959db0e0664213b17957c0333b5ec114f68
Ivan <i.romanov@protonmail.com>
2020-03-26 13:10:54 +0300

    Returned to conditinal render of service desktop and adaptive components based on redux state

--- src/Pages/HomePage/Services/Services.js
+++ src/Pages/HomePage/Services/Services.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ServicesAdaptive from 'Pages/HomePage/Services/ServicesAdaptive/ServicesAdaptive'
 import ServicesDesktop from 'Pages/HomePage/Services/ServicesDesktop/ServicesDesktop'
 import css from './Services.module.scss'
+import { useSelector } from 'react-redux'
 
 const Services = props => {
-  return (
-    <>
-      <ServicesDesktop {...props} className={css.desktop} />
-      <ServicesAdaptive {...props} className={css.adaptive} />
-    </>
-  )
+  const type = useSelector(state => state.elastic.deviceType)
+
+  return type === 'desktop'
+    ? <ServicesDesktop {...props} className={css.desktop} />
+    : <ServicesAdaptive {...props} className={css.adaptive} />
 }
 
 export default Services
